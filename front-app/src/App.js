6f80ec3a8494044e207c2ee5fcce1e285e9eadb5
mymo <106878420+mymooss@users.noreply.github.com>
2023-07-10 21:45:25 +0900

    feat

--- front-app/src/App.js
+++ front-app/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import {BrowserRouter,Routes,Route } from 'react-router-dom';
 import './App.css';
-import image from './img/1.png';
+import Main from'./main'
+import Realtime from './componnents/rtsp';
 
-function App() {
+const App = () => {
   return (
-    <div className='main' style={{backgroundColor:'#FFEBF5'}}>
-      <h1>핑크 서브웨이</h1>
-      <img src={image} alt='img'></img><br/>
-      <button>실시간 임산부 배려석 점유 현황</button><br/>
-      <button>"양보해주세요"</button>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Main />}></Route>
+          <Route path='/realtime' element={<Realtime />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ front-app/src/main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './App.css';
+import IMG from './img/1.png';
+
+const main = () => {
+  return (
+    <div className='main'>
+      <h1>핑크 서브웨이</h1>
+      <img src={IMG} alt='image'></img>
+      <Link to='/realtime'>
+        <button>실시간 임산부 배려석 점유 현황</button>
+      </Link>
+      <br />
+      <button>"양보해주세요"</button>
+    </div>
+  );
+}
+
+export default main;
