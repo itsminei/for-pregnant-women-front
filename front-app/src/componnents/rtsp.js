0965dc3cf3ddb6188f0b4ddcd14f2e29a3f11264
mymo <106878420+mymooss@users.noreply.github.com>
2023-09-15 12:01:34 +0900

    [FIX]

--- front-app/src/componnents/rtsp.js
+++ front-app/src/componnents/rtsp.js
@@ -1,67 +1,47 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { doc, getDoc } from "firebase/firestore";
+import { db } from '../fbase';
+import './Realtime.css';
 
-const Realtime = () => {
-  const [responseData, setResponseData] = useState(null);
+function Realtime() {
+  const [piData, setPiData] = useState(0);
 
-  const handleClick = () => {
-    var url = 'http://apis.data.go.kr/1613000/SubwayInfoService/getSubwaySttnAcctoSchdulList';
-    var queryParams = '?' + encodeURIComponent('serviceKey') + '=ABh0rO8gQOEEfBcY6OtLM3GfP36L6wHiFAI7G3hFJQ0A26HNv4wsBAUc0dLFrwrKYri0T0tocsvm3X6wDxLNyA%3D%3D';
-    queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1');
-    queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10');
-    queryParams += '&' + encodeURIComponent('_type') + '=' + encodeURIComponent('json');
-    queryParams += '&' + encodeURIComponent('subwayStationId') + '=' + encodeURIComponent('MTRARA1A01');
-    queryParams += '&' + encodeURIComponent('dailyTypeCode') + '=' + encodeURIComponent('01');
-    queryParams += '&' + encodeURIComponent('upDownTypeCode') + '=' + encodeURIComponent('D');
+  useEffect(() => {
+    const fetchData = async () => {
+      const docRef = doc(db, "seat", "raspi");
+      const docSnap = await getDoc(docRef);
 
-    axios.get(url + queryParams)
-      .then(response => {
-        // Update the state with the received data
-        setResponseData(response.data);
-        console.log(response);
-      })
-      .catch(error => {
-        // Handle errors
-        console.error(error);
-      });
-  };
+      if (docSnap.exists()) {
+        setPiData(docSnap.data().pi);
+      } else {
+        console.log("No such document!");
+      }
+    };
 
-  const getCurrentTime = () => {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    return `${hours}${minutes}`;
-  };
+    fetchData();
 
-  const calculateArrivalTime = (depTime) => {
-    const currentTime = getCurrentTime();
-    const timeDiff = parseInt(depTime, 10) - parseInt(currentTime, 10);
+    const intervalId = setInterval(() => {
+      fetchData();
+    }, 5000);
 
-    if (timeDiff < 0) {
-      return '이미 출발했습니다.';
-    } else {
-      const minutes = Math.floor(timeDiff / 100); // Assuming depTime format: "HHmm"
-      return `약 ${minutes}분 후 도착 예정`;
-    }
-  };
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <div>
-      <button onClick={handleClick}>API 요청 보내기</button>
-      {responseData && (
-        <div>
-          <h2>지하철 도착 예정 시간:</h2>
-          <ul>
-            {responseData.response.body.items.item.map((subway, index) => (
-              <li key={index}>
-                {`${subway.subwayStationNm}행: ${calculateArrivalTime(subway.depTime)}`}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
+      <h1>실시간 좌석 점유 현황</h1>
+      <div className="grid">
+        {[...Array(12)].map((_, index) => (
+          <div
+            key={index}
+            className={`box ${index === 2 && piData === 0 ? "red" : "gray"}`}
+          >
+            {index === 2 && piData === 0 ? "좌석이 비었습니다" : ""}
+          </div>
+        ))}
+      </div>
     </div>
   );
-};
+}
 
 export default Realtime;
--- front-app/src/fbase.js
+++ front-app/src/fbase.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth"; 
+import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,7 +12,7 @@ const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId:"ictm-1af05",
+  projectId:process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
@@ -22,3 +24,5 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 export const authService = getAuth();
+export const db = getFirestore(app);
+export const database = getDatabase(app);
